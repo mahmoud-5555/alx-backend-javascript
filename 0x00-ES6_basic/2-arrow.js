1995468dd94301effa09237f0a135f23cfb91983
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-01 02:06:52 +0300

    2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,10 +3,8 @@ export default function getNeighborhoodsList() {
 
   const self = this;
 
-  const add = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
-
-  this.addNeighborhood = add;
 }
