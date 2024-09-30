b9918428853a32bee7430d0f238c622be13cc100
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-01 01:42:49 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,7 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
+  if (trueOrFalse) return [task, task2];
 
   return [task, task2];
 }
