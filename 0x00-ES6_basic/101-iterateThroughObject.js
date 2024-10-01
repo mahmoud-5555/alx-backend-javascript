2b5280b546370fa692738b2adcb1fd1196fc0fd5
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-01 04:57:51 +0300

    100-createIteratorObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
+  const employees = []; // Array to hold employee names
 
+  for (const employee of reportWithIterator) {
+    employees.push(employee); // Add each employee name to the array
+  }
+
+  return employees.join(' | '); // Join the names with ' | ' and return
 }
