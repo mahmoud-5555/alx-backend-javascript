4ebb1fb1aca1024983b7e1624c09ba569a769a7a
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-04 00:22:06 +0300

    1-make_classrooms.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,9 +4,12 @@ export default class HolbertonCourse {
       throw new TypeError('Name must be a string');
     }
     if (typeof lenght !== 'number') {
-        throw new TypeError('Length must be a number');
-        }
-    
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(studans)) {
+      throw new TypeError('Students must be an array');
+    }
+
     this._name = name;
     this._length = lenght;
     this._students = studans;
@@ -26,20 +29,20 @@ export default class HolbertonCourse {
 
   set name(newName) {
     if (typeof newName === 'string') {
-        this._name = newName;
-    } else
-        throw new TypeError('Name must be a string');
+      this._name = newName;
+    } else throw new TypeError('Name must be a string');
   }
 
   set length(newLength) {
     if (typeof newLength === 'number') {
-        this._length = newLength;
-    } else
-        throw new TypeError('Length must be a number');
+      this._length = newLength;
+    } else throw new TypeError('Length must be a number');
   }
 
   set students(newStudents) {
-
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else throw new TypeError('Students must be an array');
     this._students = newStudents;
   }
 }
