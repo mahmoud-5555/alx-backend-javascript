d09b9828b617de19b4e54904842be9cd8b9fa5e0
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-04 00:40:21 +0300

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,4 +7,22 @@ export default class Currency {
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof (newName) === 'string') this._name = newName;
+    else throw new TypeError('Name must be a string');
+  }
+
+  set code(newCode) {
+    if (typeof (newCode) === 'string') this._code = newCode;
+    else throw new TypeError('Code must be a string');
+  }
 }
