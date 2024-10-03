9cd964f9752141efd97e0e9b2b105e1ffe6851ba
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-04 00:17:36 +0300

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, lenght, studans) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof lenght !== 'number') {
+        throw new TypeError('Length must be a number');
+        }
+    
+    this._name = name;
+    this._length = lenght;
+    this._students = studans;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+        this._name = newName;
+    } else
+        throw new TypeError('Name must be a string');
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+        this._length = newLength;
+    } else
+        throw new TypeError('Length must be a number');
+  }
+
+  set students(newStudents) {
+
+    this._students = newStudents;
+  }
+}
