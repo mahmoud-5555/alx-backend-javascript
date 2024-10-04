6bea341e4fc84df3dc454220b43695316fcc95da
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-10-04 03:06:44 +0300

    4-pricing.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
     if (new.target === Building) {
       throw new Error('Cannot instantiate an abstract class.');
     }
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
@@ -22,7 +22,5 @@ export default class Building {
 
   // Abstract method, should be overridden in subclasses
   // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+
 }
